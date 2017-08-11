5b712648a41e65455875722d26e55a88f7ba6cf1
Roberto <raristides.caamano@gmail.com>
2017-08-10 20:39:46 -0400

    First Commit

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Quote;
+
+class QuoteController extends Controller
+{
+    public function postQuote(Request $request) {
+        $quote =  new Quote();
+        $quote->content = $request->input('content');
+        $quote->save();
+        return response()->json(['quote' => $quote], 201);
+    }
+
+    public function getQuotes() {
+        $quotes = Quote::all();
+        $response = [
+            'quotes' => $quotes
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function putQuote(Request $request, $id) {
+        $quote = Quote::find($id);
+        if(!$quote) {
+            return response()->json(['message' => 'No se encontró el documento'], 404);
+        }
+        $quote->content = $request->input('content');
+        $quote->save();
+        return response()->json(['quote' => $quote], 200);
+    }
+
+    public function deleteQuote($id) {
+        $quote = Quote::find($id);
+        $quote->delete();
+        return response()->json(['message' => 'Frase eliminada'], 200);
+    }
+}
